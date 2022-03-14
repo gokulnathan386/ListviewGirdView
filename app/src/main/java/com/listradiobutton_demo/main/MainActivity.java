a93e5de9de89b27fdb13fc7a2e7c835c846e60fd
Pasubathi <73518189+Pasubathi@users.noreply.github.com>
2022-03-14 18:37:30 +0530

    listview single choose Girdview

---
+++ app/src/main/java/com/listradiobutton_demo/main/MainActivity.java
@@ -0,0 +1,40 @@
+package com.listradiobutton_demo.main;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+import com.listradiobutton_demo.R;
+import com.listradiobutton_demo.adapter.ViewPagerAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+        setupViewPager(viewPager);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);//setting tab over viewpager
+    }
+
+    //Setting View Pager
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFrag(new ListViewFragment(), "ListView");
+        adapter.addFrag(new GridViewFragment(), "GridView");
+        adapter.addFrag(new RecyclerViewFragment(), "RecyclerView");
+        viewPager.setAdapter(adapter);
+    }
+
+
+}
